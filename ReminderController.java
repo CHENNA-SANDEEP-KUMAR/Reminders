8c2473c543dec9d003f2a19305f1820b4b2f706e
CHENNA SANDEEP KUMAR <chenna.sandeep95@gmail.com>
2025-08-10 21:32:31 +0530

    Update ReminderController.java

--- ReminderController.java
+++ ReminderController.java
@@ -1,28 +1,30 @@
 package com.example.reminderapp.controller;
 
 import com.example.reminderapp.model.Reminder;
+import com.example.reminderapp.service.ReminderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class ReminderController {
 
-    private final List<Reminder> reminders = new ArrayList<>();
+    private final ReminderService reminderService;
+
+    public ReminderController(ReminderService reminderService) {
+        this.reminderService = reminderService;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("reminders", reminders);
+        model.addAttribute("reminders", reminderService.getAllReminders());
         model.addAttribute("newReminder", new Reminder());
         return "index";
     }
 
     @PostMapping("/add")
     public String addReminder(@ModelAttribute Reminder reminder) {
-        reminders.add(reminder);
+        reminderService.addReminder(reminder);
         return "redirect:/";
     }
 }
