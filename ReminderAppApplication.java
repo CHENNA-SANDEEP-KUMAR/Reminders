16c9b8561b6a45930b3d2bab83eaefd76f4efc29
CHENNA SANDEEP KUMAR <chenna.sandeep95@gmail.com>
2025-08-10 21:30:18 +0530

    Update ReminderAppApplication.java

--- ReminderAppApplication.java
+++ ReminderAppApplication.java
@@ -2,8 +2,10 @@ package com.example.reminderapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // Enables Spring's scheduled tasks
 public class ReminderAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReminderAppApplication.class, args);
